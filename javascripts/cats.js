4ef2d0badade3c58c0b0b5f07e83262dcc138237
Juno Lee <lejuno80@gmail.com>
2017-09-09 12:44:40 -0500

    updated dom

--- javascripts/cats.js
+++ javascripts/cats.js
@@ -1,1 +1,18 @@
-console.log("cats");
+var CatStore = ((oldCatStore) => {
+  let cats = []; //cannot overwrite array so switching to let
+
+ oldCatStore.getCats = () => {
+  	return cats;
+  }
+ oldCatStore.setAllCats = (allMyCats) => {
+  	cats = allMyCats;
+  };
+
+	return oldCatStore;
+})(CatStore || {});
+
+// var crap = (()=> {
+
+// })();
+
+
